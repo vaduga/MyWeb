271fa962deefb70cf7d10e1e29a0f907276b82fe
vaduga <arbitr38@gmail.com>
2022-02-11 17:25:11 +0800

    list in view from DB

--- WebAppMVC/Controllers/CategoryController.cs
+++ WebAppMVC/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.Data;
+using WebAppMVC.Models;
 
 namespace WebAppMVC.Controllers;
 
 public class CategoryController : Controller
 {
     // GET
+
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
     public IActionResult Index()
     {
-        return View();
+
+        IEnumerable<Category> objList = _db.Category;
+        return View(objList);
     }
 }
+
